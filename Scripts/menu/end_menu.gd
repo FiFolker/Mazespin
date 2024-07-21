7edeeff8aacef2776e731834494c9771eb71f1ec
FiFolker <elouan.lemerle@gmail.Com>
2024-07-22 00:19:44 +0200

    add grab focus to end menus

--- Scripts/menu/end_menu.gd
+++ Scripts/menu/end_menu.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+@export var default_button_to_grab : Button
+
+func _ready():
+	if OptionsValues.input == OptionsValues.INPUT.CONTROLLER and default_button_to_grab != null:
+		default_button_to_grab.grab_focus()
+
 func _on_menu_btn_button_down():
 	get_tree().change_scene_to_file("res://Scenes/menus/main_menu.tscn")
 
